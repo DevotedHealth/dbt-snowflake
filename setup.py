64a8c9b576e28d364f7476735595aa0f24fd41ce
Adam Boscarino <aboscarino@devoted.com>
2022-02-24 23:41:14 +0000

    remove version check for fork

--- setup.py
+++ setup.py
@@ -30,33 +30,9 @@ this_directory = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(this_directory, "README.md")) as f:
     long_description = f.read()
 
-
-# get this package's version from dbt/adapters/<name>/__version__.py
-def _get_plugin_version_dict():
-    _version_path = os.path.join(
-        this_directory, "dbt", "adapters", "snowflake", "__version__.py"
-    )
-    _semver = r"""(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"""
-    _pre = r"""((?P<prekind>a|b|rc|dvtd)(?P<pre>\d+))?"""
-    _version_pattern = fr"""version\s*=\s*["']{_semver}{_pre}["']"""
-    with open(_version_path) as f:
-        match = re.search(_version_pattern, f.read().strip())
-        if match is None:
-            raise ValueError(f"invalid version at {_version_path}")
-        return match.groupdict()
-
-
-# require a compatible minor version (~=), prerelease if this is a prerelease
-def _get_dbt_core_version():
-    parts = _get_plugin_version_dict()
-    minor = "{major}.{minor}.0".format(**parts)
-    pre = parts["prekind"] + "1" if parts["prekind"] else ""
-    return f"{minor}{pre}"
-
-
 package_name = "dbt-snowflake"
 package_version = "1.0.0dvtd"
-dbt_core_version = _get_dbt_core_version()
+dbt_core_version = "1.0.0"
 description = """The Snowflake adapter plugin for dbt"""
 
 setup(
